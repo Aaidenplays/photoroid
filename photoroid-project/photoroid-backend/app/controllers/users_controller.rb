22108a420ccb1fb1758c5d906dc2c213d0042458
github username <github email address>
2020-03-23 16:39:15 -0500

    JSON for backend working

--- photoroid-project/photoroid-backend/app/controllers/users_controller.rb
+++ photoroid-project/photoroid-backend/app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+  # def index
+  #   trainers = Trainer.all
+
+  #   render json: trainers
+  # end
+  
   def index
-    
+    users = User.all
+    render json: users
   end
 end
