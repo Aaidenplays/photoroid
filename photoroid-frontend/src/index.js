8e819810ac616bfd7802207c78b3567bbf3b55d4
github username <github email address>
2020-03-24 12:00:23 -0500

    added navigation via headers. Created addfriendbtn

--- photoroid-frontend/src/index.js
+++ photoroid-frontend/src/index.js
@@ -1,7 +1,16 @@
+const headers = {
+  'Content-Type': 'application/json',
+  'Accept':  'application/json'
+}
+
+let current_user;
+
 let totalUsers;
 document.addEventListener('DOMContentLoaded', ()=>{
       console.log('Hey there Cowboy!');
-      loadFriends();
+      headerHandler();
+      // getUsers();
+      // loadFriends();
 el('new-login').addEventListener('click', ()=>{
 console.log('You clicked the button')
       let div = el('new-user');
@@ -28,7 +37,6 @@ el('user-login').addEventListener('click', ()=>{
         <input type="text" id="password" name="password">
         <input type='submit' value='Submit'>
                       </form>`;
-
                     });
 
 
@@ -40,6 +48,7 @@ function loadFriends(){
   .then(json =>{
     totalUsers = json.length
     el('total-users').innerText = `Total Users: ${totalUsers}`;
+    el('users').innerHTML = '';
     for(const i in json){
       let ul = el('users');
       let userLi = document.createElement('li');
@@ -47,11 +56,100 @@ function loadFriends(){
                           <p>${json[i].bio}</p>`;
       ul.appendChild(userLi);
 
-
     };
   })
 
 }
 function el(id){
   return document.getElementById(id);
+}
+
+
+
+
+
+/* Event Handlers */
+const postFriendRequest = (data,target) => {
+  console.log(`HEADERS: ${headers}`)
+    console.log(data)
+    console.log(`target: ${target}`)
+    const u1 = data[0];
+    const u2 = target;
+    console.log(`u1: ${u1.id}`);
+    console.log(`u2: ${u2}`);
+    fetch('http://localhost:3000/friend_requests',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept':  'application/json'
+      },
+      body: JSON.stringify({
+        requestor_id: u1.id,
+        receiver_id: u2,
+        status: "pending"
+      })
+    })
+}
+const handleAddFriend = (e) => {
+  // console.log(`e: ${e.target.dataset.id}`)
+  fetch('http://localhost:3000/users')
+  .then(resp => resp.json())
+  .then(data => postFriendRequest(data,e.target.dataset.id))
+
+  //replace all u1 with current_user
+
+}
+
+/* Header Handling */
+const headerHandler = () => {
+  el('feed-header');
+  el('my-boards-header');
+  el('friends-header').addEventListener("click",(e)=>{ 
+    e.preventDefault();
+    console.log("ITS HAPPENING")
+    loadFriends()});
+  el('users-header').addEventListener("click", (e) => { 
+    e.preventDefault();
+    getUsers();
+  });
+  el('create-boards-header')
+  
+}
+
+/* Rendering Functions */
+
+
+
+const renderUsers = (users) => {
+  const usersList = el("users");
+  usersList.innerHTML = '';
+  users.forEach(user => {
+    /* declaring empty elements */
+    let li = document.createElement('li');
+    let name = document.createElement('h2');
+    let bio = document.createElement('p');
+    /* initializing */
+    name.innerText = user.name;
+    bio.innerText = user.bio;
+    /* add friend button */
+    addFriendBtn = document.createElement('button');
+    addFriendBtn.innerText = 'Add Friend';
+    addFriendBtn.style=('color:blue; background-color:orange');
+    addFriendBtn.dataset.id = (user.id);
+    addFriendBtn.addEventListener("click", (e) => handleAddFriend(e));
+
+    /* appending elements to list */
+    li.append(name);
+    li.append(bio);
+    li.append(addFriendBtn);
+    /* appending list to container */
+    usersList.append(li);
+  });
+}
+
+
+const getUsers = () =>{
+  fetch('http://localhost:3000/users')
+  .then(resp => resp.json())
+  .then(data => renderUsers(data))
 }
--- photoroid-project/photoroid-backend/app/controllers/friend_requests_controller.rb
+++ photoroid-project/photoroid-backend/app/controllers/friend_requests_controller.rb
@@ -3,4 +3,20 @@ class FriendRequestsController < ApplicationController
         friend_requests =  FriendRequest.all
         render json: friend_requests
       end
+
+      def create
+        FriendRequest.create(requestor_id: params[:requestor_id], receiver_id: params[:receiver_id], status: params[:status])
+      end
+
+    #   def create
+    #     trainer = Trainer.find(params["pokemon"]["trainer_id"])
+        
+    #     if trainer.pokemons.count >= 6 
+    #       render json: { error: "Party is Full!"}, status: 403
+    #     else 
+    #       pokemon = Pokemon.create(nickname: Faker::Name.first_name, species: Faker::Games::Pokemon.name, trainer: trainer)
+    #       render json: pokemon, status: 200
+    #     end
+    #   end
+    
 end
